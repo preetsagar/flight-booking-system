7cffa064f0ce0b942da6bf041dc54f11bb90a389
preetsagar <ps792001@gmail.com>
2023-04-20 07:22:05 +0530

    Updated server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ mongoose
     console.log("Connected With The DataBASE");
 
     //   After Connecting with the database now start listening
-    const PORT = 3000 || process.env.PORT;
+    const PORT = process.env.PORT || 3000;
     app.listen(PORT, () => {
       console.log(`Server Running on Port ${PORT}`);
     });
